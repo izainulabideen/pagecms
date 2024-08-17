e097fca2207511c91fe44700c86ec59547e38d35
izainulabideen <zainrao417@gmail.com>
2024-08-17 15:27:28 +0500

    some fixes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -12,5 +11,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    outDir: 'docs',
   }
 })
